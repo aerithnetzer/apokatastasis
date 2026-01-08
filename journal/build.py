ebe9b2e995c63ca8ff61d71173f3ac0fce353d2c
aerithnetzer <aerithnetzer@outlook.com>
2026-01-07 22:00:27 -0600

    Add basic logging

--- journal/build.py
+++ journal/build.py
@@ -1,51 +1,58 @@
+import logging
 import os
 import frontmatter as fm
 import shutil
 import markdown
 from jinja2 import Environment, FileSystemLoader
+from loguru import logger
+import sys
+# Setup global vars
+
+logger.remove()
+
+_ = logger.add(sys.stderr, level="ERROR")
+
+_ = logger.add("/".join([os.getcwd(), ".logs", "apok.log"]))
 
 CONTENT_DIR = "content"
 TEMPLATE_DIR = "templates"
 OUTPUT_DIR = "public"
 STATIC_DIR = "static"
-JATS_PATH = "macc.yaml"
+JATS_PATH = "apok.yaml"
 
-env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+
+env = Environment(
+    loader=FileSystemLoader(TEMPLATE_DIR)
+)  # Setup environment with TEMPLATE_DIR
 md = markdown.Markdown(extensions=["fenced_code", "tables"])
 
+
 def build_page(src: str, dst: str):
+    logger.info(f"Building content {src}")
+    logger.info(f"Loading frontmatter {src}")
     post = fm.load(src)
+
+    logger.debug(f"Type of `post` {type(post)}")
     site = fm.load(JATS_PATH)
+
     metadata = post.metadata
     site_meta = site.metadata
-    print("Site meta:", site_meta)
+
     content = post.content
     template_names: str | list[str] | object = metadata.get("template", "base.html")
-    
-    if dst.endswith(".xml"):
 
+    if dst.endswith(".xml"):
         template = env.get_template("article-jats.xml")
 
-        rendered = template.render(
-            content=content,
-            metadata=metadata,
-            site=site_meta
-
-        )
+        rendered = template.render(content=content, metadata=metadata, site=site_meta)
 
         os.makedirs(os.path.dirname(dst), exist_ok=True)
         with open(dst, "w") as f:
             _ = f.write(rendered)
     else:
-
         template = env.get_template("article.html")
 
-        rendered = template.render(
-            content=content,
-            metadata=metadata,
-            site=site_meta
-
-        )
+        rendered = template.render(content=content, metadata=metadata, site=site_meta)
 
         os.makedirs(os.path.dirname(dst), exist_ok=True)
         with open(dst, "w") as f:
@@ -68,8 +75,6 @@ def build_site():
                 build_page(src, dst_html)
                 build_page(src, dst_xml)
 
-var = ("In the age of super boredom hype", 
-       "and mediocrity celebrate relentlness menace to society")
 
 if __name__ == "__main__":
     build_site()
